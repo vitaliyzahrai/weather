2d2742640c54c6bb4469299768245d431efbed11
vitaliyzahrai <vitaliy.zahrai@gmail.com>
2021-02-06 00:14:43 +0200

    first commit

---
+++ js/slick.js
@@ -0,0 +1,22 @@
+
+
+
+  // $('.slideshow').slick({
+  //   variableWidth: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   adaptiveHeight: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 4000,
+  //   pauseOnHover:true,
+  //   pauseOnFocus:true,
+  //   focusOnSelect: true
+
+  // });
+  
+
+
+
+
+
+
